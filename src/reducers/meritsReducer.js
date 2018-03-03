b58ff5c380076c759c651ea4feda88b60a2ee114
Bill Sorensen <baldgoth@gmail.com>
2018-03-03 17:06:00 -0600

    Convert merits reducer to immer

--- src/reducers/meritsReducer.js
+++ src/reducers/meritsReducer.js
@@ -1,19 +1,27 @@
+import produce from 'immer';
 import initialState from './initialState';
 import * as types from '../constants/actionTypes';
 
-export default (state = initialState.character.merits, action) => {
-  switch (action.type) {
-    case types.ADD_MERIT:
-      const newState = state.slice();
-      newState.push(action.payload);
-      return newState;
-    case types.REMOVE_MERIT:
-      const { name } = action.payload;
-      return state.filter(x => x.name !== name);
-    case types.UPDATE_CLAN:
-      // reset, as there are clan-specific merits
-      return initialState.character.merits;
-    default:
-      return state;
-  }
-};
+export default (state = initialState.character.merits, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case types.ADD_MERIT:
+        draft.push(action.payload);
+        break;
+      case types.REMOVE_MERIT:
+        const { name } = action.payload;
+
+        for (let i = 0; i < draft.length; i++) {
+          if (draft[i].name === name) {
+            draft.splice(i, 1);
+            break;
+          }
+        }
+        break;
+      case types.UPDATE_CLAN:
+        // reset, as there are clan-specific merits
+        return initialState.character.merits;
+      default:
+        break;
+    }
+  });
