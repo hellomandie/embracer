5b5191c3365e1a83305e186d401eccdc65960d6f
Bill Sorensen <baldgoth@gmail.com>
2018-03-03 17:09:22 -0600

    Convert mode reducer to immer

--- src/reducers/modeReducer.js
+++ src/reducers/modeReducer.js
@@ -1,11 +1,14 @@
+import produce from 'immer';
 import initialState from './initialState';
 import * as types from '../constants/actionTypes';
 
-export default (state = initialState.mode, action) => {
-  switch (action.type) {
-    case types.TOGGLE_PENCIL_ERASER_MODE:
-      return { ...state, isEraser: !state.isEraser };
-    default:
-      return state;
-  }
-};
+export default (state = initialState.mode, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case types.TOGGLE_PENCIL_ERASER_MODE:
+        draft.isEraser = !draft.isEraser;
+        break;
+      default:
+        break;
+    }
+  });
