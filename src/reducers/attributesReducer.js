7b53d4af9fcccb8e7925e0b885e150fb640fb0e6
Bill Sorensen <baldgoth@gmail.com>
2018-03-06 20:13:06 -0600

    Convert attributes reducer to immer

--- src/reducers/attributesReducer.js
+++ src/reducers/attributesReducer.js
@@ -1,3 +1,4 @@
+import produce from 'immer';
 import initialState from './initialState';
 import * as types from '../constants/actionTypes';
 import { attributeMaxDots } from '../constants/characterOptions';
@@ -9,39 +10,38 @@ import {
 
 const isAttributes = category => category === 'attributes';
 
-export default (state = initialState.character.attributes, action) => {
-  let category, trait, dotsFromRank;
+export default (state = initialState.character.attributes, action) =>
+  produce(state, draft => {
+    let category, trait, dotsFromRank;
 
-  switch (action.type) {
-    case types.SET_RANK:
-      ({ category, trait, dotsFromRank } = action.payload);
+    switch (action.type) {
+      case types.SET_RANK:
+        ({ category, trait, dotsFromRank } = action.payload);
 
-      if (!isAttributes(category)) {
-        return state;
-      }
+        if (isAttributes(category)) {
+          return setDotsFromRank(draft, trait, dotsFromRank, attributeMaxDots);
+        }
+        break;
+      case types.SET_FOCUS:
+        const { attribute, focus } = action.payload;
 
-      return setDotsFromRank(state, trait, dotsFromRank, attributeMaxDots);
-    case types.SET_FOCUS:
-      const { attribute, focus } = action.payload;
+        draft[attribute].focus = focus;
+        break;
+      case types.PURCHASE_DOT:
+        ({ category, trait } = action.payload);
 
-      return { ...state, [attribute]: { ...state[attribute], focus: focus } };
-    case types.PURCHASE_DOT:
-      ({ category, trait } = action.payload);
+        if (isAttributes(category)) {
+          return addPurchasedDot(draft, trait, attributeMaxDots);
+        }
+        break;
+      case types.UNPURCHASE_DOT:
+        ({ category, trait } = action.payload);
 
-      if (!isAttributes(category)) {
-        return state;
-      }
-
-      return addPurchasedDot(state, trait, attributeMaxDots);
-    case types.UNPURCHASE_DOT:
-      ({ category, trait } = action.payload);
-
-      if (!isAttributes(category)) {
-        return state;
-      }
-
-      return removePurchasedDot(state, trait, true);
-    default:
-      return state;
-  }
-};
+        if (isAttributes(category)) {
+          return removePurchasedDot(draft, trait, true);
+        }
+        break;
+      default:
+        break;
+    }
+  });
